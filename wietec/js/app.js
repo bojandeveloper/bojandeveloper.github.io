dcd3a8bd99bd078bf3be7d2b2d0c0946e47b70fc
bojandeveloper <jovanovicbojan.rs@gmail.com>
2023-03-01 20:45:35 +0100

    Add new pages

--- wietec/js/app.js
+++ wietec/js/app.js
@@ -5,6 +5,7 @@
 const DOM = {
   toggleBtn: document.querySelector(".toggle-button"),
   navEl: document.querySelector(".nav"),
+  subMenu: document.querySelectorAll(".has-sub-menu > a"),
 };
 
 /////////////////////////////////////
@@ -17,3 +18,12 @@ const toggleNav = function () {
 /////////////////////////////////////
 // Events
 DOM.toggleBtn.addEventListener("click", toggleNav);
+
+DOM.subMenu.forEach((el) => {
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (window.innerWidth < 993) {
+      el.parentElement.classList.toggle("active");
+    }
+  });
+});
